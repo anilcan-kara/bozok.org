dc6ce3d090e433e51175b39394c858944b49f86e
Anilcan Kara <anilcankara.com@gmail.com>
2024-10-22 16:34:28 +0300

    Remove unnecessary VercelIcon import and element. Improve code readability.

--- components/custom/overview.jsx
+++ components/custom/overview.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 
-import { MessageIcon, VercelIcon } from "./icons";
+import { MessageIcon } from "./icons";
 
 export const Overview = () => {
     return (
@@ -14,10 +14,9 @@ export const Overview = () => {
         >
             <div className="border rounded-lg p-6 flex flex-col gap-4 text-zinc-500 text-sm dark:text-zinc-400 dark:border-zinc-700">
                 <p className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50">
-                    <VercelIcon />
-                    <span>+</span>
                     <MessageIcon />
                 </p>
+
                 <p>
                     This is Bozok University Chatbot built from Anilcan Kara. It uses the{" "}
                     <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{" "}
