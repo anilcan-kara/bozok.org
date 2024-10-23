18b74caf15244abe4791abf45beb41a961a7f302
Anilcan Kara <anilcankara.com@gmail.com>
2024-10-23 08:50:31 +0300

    Add Link import for navigation
    Imported Link component from 'next/link' for navigation.

--- components/custom/overview.jsx
+++ components/custom/overview.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 import { MessageIcon } from './icons';
-import Link from 'next/link';
 
 export const Overview = () => {
     return (
