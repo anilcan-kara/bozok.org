0c3a8a9ced181ff06fdc72c8ead0226eb5d93ff2
Anilcan Kara <anilcankara.com@gmail.com>
2024-11-21 11:31:13 +0300

    Update auth config for public file access
    - Added support for various public file extensions.
    - Cleared out unused providers array.
    - Updated authorization logic to allow access to public files.

--- app/(auth)/auth.config.js
+++ app/(auth)/auth.config.js
@@ -1,30 +1,30 @@
+const publicFileExtensions = ['xml', 'txt', 'json', 'rss', 'atom', 'csv', 'ics', 'yaml', 'text', 'png', 'jpg', 'jpeg', 'gif', 'webp', 'svg', 'ico', 'bmp', 'tif', 'tiff', 'css', 'less', 'scss', 'styl', 'js', 'mjs', 'jsx', 'ts', 'tsx', 'php', 'asp', 'aspx', 'jsonld'];
+
 export const authConfig = {
     pages: {
         signIn: "/login",
         newUser: "/",
     },
-    providers: [
-        // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
-        // while this file is also used in non-Node.js environments
-    ],
+    providers: [],
     callbacks: {
         authorized({ auth, request: { nextUrl } }) {
             let isLoggedIn = !!auth?.user;
             let isOnChat = nextUrl.pathname.startsWith("/");
             let isOnRegister = nextUrl.pathname.startsWith("/register");
             let isOnLogin = nextUrl.pathname.startsWith("/login");
+            let isPublic = publicFileExtensions.some((e) => nextUrl.pathname.endsWith(e));
 
             if (isLoggedIn && (isOnLogin || isOnRegister)) {
                 return Response.redirect(new URL("/", nextUrl));
             }
 
-            if (isOnRegister || isOnLogin) {
-                return true; // Always allow access to register and login pages
+            if (isOnRegister || isOnLogin || isPublic) {
+                return true;
             }
 
             if (isOnChat) {
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
+                return false;
             }
 
             if (isLoggedIn) {
