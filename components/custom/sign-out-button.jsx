3a757e540ceca30be6bebd78dbf750e1fe84d733
Anilcan Kara <anilcankara.com@gmail.com>
2024-12-09 00:12:40 +0300

    Enhance SignOutButton styling for improved user experience and accessibility

--- components/custom/sign-out-button.jsx
+++ components/custom/sign-out-button.jsx
@@ -9,7 +9,7 @@ function SignOutButton() {
                 await signOut();
             }}
         >
-            <button type="submit" className="w-full text-left px-1 py-0.5 text-red-500 text-xs">
+            <button type="submit" className="w-full text-left px-2 py-1.75 active:text-red-500 text-xs bg-gray-200/60 rounded-md hover:bg-gray-200 active:bg-gray-100">
                 Çıkış
             </button>
         </form>
